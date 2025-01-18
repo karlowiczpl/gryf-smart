63d7f8c320fd983fe25ed68b670823a682149cc8
karlowiczpl <karol.kar1@outlook.com>
2025-01-18 19:13:36 +0100

    alarm 0.3

--- alarm.py
+++ alarm.py
@@ -1,19 +1,25 @@
 import logging
 from homeassistant.components.alarm_control_panel import AlarmControlPanelEntity
-from homeassistant.const import STATE_ALARM_ARMED_AWAY, STATE_ALARM_ARMED_HOME, STATE_ALARM_DISARMED, STATE_ALARM_TRIGGERED
+from homeassistant.const import (
+    STATE_ALARM_ARMED_AWAY,
+    STATE_ALARM_ARMED_HOME,
+    STATE_ALARM_DISARMED,
+    STATE_ALARM_TRIGGERED,
+)
 
 _LOGGER = logging.getLogger(__name__)
 
 async def async_setup_platform(hass, config, async_add_entities, discovery_info=None):
     """Set up the custom alarm control panel."""
     async_add_entities([CustomAlarmEntity()])
-
 class CustomAlarmEntity(AlarmControlPanelEntity):
     """Representation of a custom alarm control panel."""
 
     def __init__(self):
         """Initialize the alarm panel."""
         self._state = STATE_ALARM_DISARMED
+        self._is_active = False  # Indicates whether the alarm is currently active
+        self._code = "1234"  # Default PIN code, change it as needed
 
     @property
     def name(self):
@@ -30,26 +36,46 @@ class CustomAlarmEntity(AlarmControlPanelEntity):
         """Return the list of supported features."""
         return 0
 
+    @property
+    def extra_state_attributes(self):
+        """Return additional attributes."""
+        return {
+            "is_active": self._is_active,
+        }
+
     def alarm_disarm(self, code=None):
         """Send disarm command."""
-        _LOGGER.info("Disarming alarm")
-        self._state = STATE_ALARM_DISARMED
-        self.schedule_update_ha_state()
+        if code == self._code:
+            _LOGGER.info("Disarming alarm with correct code")
+            self._state = STATE_ALARM_DISARMED
+            self._is_active = False
+            self.schedule_update_ha_state()
+        else:
+            _LOGGER.warning("Failed disarming attempt with incorrect code")
 
     def alarm_arm_home(self, code=None):
         """Send arm home command."""
-        _LOGGER.info("Arming alarm in home mode")
-        self._state = STATE_ALARM_ARMED_HOME
-        self.schedule_update_ha_state()
+        if code == self._code:
+            _LOGGER.info("Arming alarm in home mode with correct code")
+            self._state = STATE_ALARM_ARMED_HOME
+            self._is_active = True
+            self.schedule_update_ha_state()
+        else:
+            _LOGGER.warning("Failed arming attempt with incorrect code")
 
     def alarm_arm_away(self, code=None):
         """Send arm away command."""
-        _LOGGER.info("Arming alarm in away mode")
-        self._state = STATE_ALARM_ARMED_AWAY
-        self.schedule_update_ha_state()
+        if code == self._code:
+            _LOGGER.info("Arming alarm in away mode with correct code")
+            self._state = STATE_ALARM_ARMED_AWAY
+            self._is_active = True
+            self.schedule_update_ha_state()
+        else:
+            _LOGGER.warning("Failed arming attempt with incorrect code")
 
     def alarm_trigger(self, code=None):
         """Trigger the alarm."""
         _LOGGER.info("Alarm triggered")
         self._state = STATE_ALARM_TRIGGERED
+        self._is_active = True
         self.schedule_update_ha_state()

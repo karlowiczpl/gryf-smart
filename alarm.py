1a7f40a82349b442b08062c6037a1a195fc67a83
karlowiczpl <karol.kar1@outlook.com>
2025-01-18 19:31:05 +0100

    alarm 0.6

--- alarm.py
+++ alarm.py
@@ -1,19 +1,34 @@
 import logging
-from homeassistant.components.alarm_control_panel import AlarmControlPanelEntity
-from homeassistant.const import STATE_ALARM_ARMED_AWAY, STATE_ALARM_ARMED_HOME, STATE_ALARM_DISARMED, STATE_ALARM_TRIGGERED
+from homeassistant.components.alarm_control_panel import AlarmControlPanelEntity, AlarmControlPanelEntityFeature
+from homeassistant.const import (
+    STATE_ALARM_ARMED_AWAY,
+    STATE_ALARM_ARMED_HOME,
+    STATE_ALARM_DISARMED,
+    STATE_ALARM_TRIGGERED,
+)
 
 _LOGGER = logging.getLogger(__name__)
 
-def setup_platform(hass, config, add_entities, discovery_info=None):
+async def async_setup_platform(hass, config, async_add_entities, discovery_info=None):
     """Set up the custom alarm control panel."""
-    add_entities([CustomAlarmEntity()])
+    async_add_entities([CustomAlarmEntity()])
 
 class CustomAlarmEntity(AlarmControlPanelEntity):
     """Representation of a custom alarm control panel."""
 
+    _attr_should_poll = False
+    _attr_code_arm_required = False
+
     def __init__(self):
         """Initialize the alarm panel."""
         self._state = STATE_ALARM_DISARMED
+        self._is_active = False
+        self._code = "1234"  # Default PIN code, change it as needed
+        self._supported_features = (
+            AlarmControlPanelEntityFeature.ARM_HOME
+            | AlarmControlPanelEntityFeature.ARM_AWAY
+            | AlarmControlPanelEntityFeature.ARM_NIGHT
+        )
 
     @property
     def name(self):
@@ -27,29 +42,49 @@ class CustomAlarmEntity(AlarmControlPanelEntity):
 
     @property
     def supported_features(self):
-        """Return the list of supported features."""
-        return 0
+        """Return the supported features."""
+        return self._supported_features
 
-    def alarm_disarm(self, code=None):
+    @property
+    def extra_state_attributes(self):
+        """Return additional attributes."""
+        return {
+            "is_active": self._is_active,
+        }
+
+    async def async_alarm_disarm(self, code=None):
         """Send disarm command."""
-        _LOGGER.info("Disarming alarm")
-        self._state = STATE_ALARM_DISARMED
-        self.schedule_update_ha_state()
+        if code == self._code:
+            _LOGGER.info("Disarming alarm with correct code")
+            self._state = STATE_ALARM_DISARMED
+            self._is_active = False
+            self.async_write_ha_state()
+        else:
+            _LOGGER.warning("Failed disarming attempt with incorrect code")
 
-    def alarm_arm_home(self, code=None):
+    async def async_alarm_arm_home(self, code=None):
         """Send arm home command."""
-        _LOGGER.info("Arming alarm in home mode")
-        self._state = STATE_ALARM_ARMED_HOME
-        self.schedule_update_ha_state()
+        if code == self._code:
+            _LOGGER.info("Arming alarm in home mode with correct code")
+            self._state = STATE_ALARM_ARMED_HOME
+            self._is_active = True
+            self.async_write_ha_state()
+        else:
+            _LOGGER.warning("Failed arming attempt with incorrect code")
 
-    def alarm_arm_away(self, code=None):
+    async def async_alarm_arm_away(self, code=None):
         """Send arm away command."""
-        _LOGGER.info("Arming alarm in away mode")
-        self._state = STATE_ALARM_ARMED_AWAY
-        self.schedule_update_ha_state()
+        if code == self._code:
+            _LOGGER.info("Arming alarm in away mode with correct code")
+            self._state = STATE_ALARM_ARMED_AWAY
+            self._is_active = True
+            self.async_write_ha_state()
+        else:
+            _LOGGER.warning("Failed arming attempt with incorrect code")
 
-    def alarm_trigger(self, code=None):
+    async def async_alarm_trigger(self, code=None):
         """Trigger the alarm."""
         _LOGGER.info("Alarm triggered")
         self._state = STATE_ALARM_TRIGGERED
-        self.schedule_update_ha_state()
+        self._is_active = True
+        self.async_write_ha_state()

4ab5eabd5ca5835d31a084ddfea86181f976c5bd
karlowiczpl <karol.kar1@outlook.com>
2025-01-20 16:40:01 +0100

    0.11

--- lights.py
+++ lights.py
@@ -4,28 +4,8 @@ from homeassistant.components.light import LightEntity
 def setup_platform(hass, config, add_entities, discovery_info=None):
     """Set up the custom alarm control panel."""
     add_entities([MyLightEntity("entity_light")])
-# Klasa bazowa dziedzicząca z Entity
-class MyBaseEntity(Entity):
-    def __init__(self, name):
-        self._name = name
-        self._custom_state = "default"
-
-    @property
-    def name(self):
-        """Zwraca nazwę encji."""
-        return self._name
-
-    @property
-    def custom_state(self):
-        """Zwraca niestandardowy stan."""
-        return self._custom_state
-
-    def update_custom_state(self, new_state):
-        """Aktualizuje niestandardowy stan."""
-        self._custom_state = new_state
-
-# Klasa specyficzna dziedzicząca z LightEntity i MyBaseEntity
-class MyLightEntity(LightEntity, MyBaseEntity):
+    
+class MyLightEntity(LightEntity):
     def __init__(self, name):
         # Wywołanie konstruktorów obu klas bazowych
         MyBaseEntity.__init__(self, name)

5f280525772a61bc9afedd2c280b5d02543402db
karlowiczpl <karol.kar1@outlook.com>
2025-01-20 17:36:09 +0100

    0.11

--- light.py
+++ light.py
@@ -4,16 +4,21 @@ from homeassistant.components.light import LightEntity
 def setup_platform(hass, config, add_entities, discovery_info=None):
     """Set up the custom alarm control panel."""
     add_entities([MyLightEntity("entity_light")])
+
+class MyBaseEntity(Entity):
+    @property
+    def name(self):
+        return "entity_light"
     
-class MyLightEntity(LightEntity):
+class MyLightEntity(LightEntity , MyBaseEntity):
     def __init__(self, name):
         # Wywołanie konstruktorów obu klas bazowych
         self._is_on = False
         self._name = name
 
-    @property
-    def name(self):
-        return self._name
+    # @property
+    # def name(self):
+    #     return self._name
 
     @property
     def is_on(self):
